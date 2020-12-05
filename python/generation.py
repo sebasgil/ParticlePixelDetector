c3d765954f0720072902bc766be411a61ff4cc0b
Massin Guerdi <massinguerdi@web.de>
2020-12-05 02:58:55 +0100

    implement most of generation

--- python/generation.py
+++ python/generation.py
@@ -9,25 +9,78 @@ class ParticlePath:
     """
     A single path that a particle takes through the detector.
     """
-    def __init__(self):
-        pass
+    def __init__(self, origin, velocity):
+        self._origin = origin
+        self._velocity = velocity
+    
+    def get_position_at_time(self, time):
+        """
+        Get particle position along it's path at a given point in time
+        Assuming the particles starts moving at time = 0
+        """
+        return self._origin + time * self._velocity
 
+    def intersection_time_with_plane(self, planeNormal, planePoint):
+        # TODO: consider exceptions instead of returning None
+        """
+        WARNING: This function may return None
+
+        Return the point in time, when the ray intersects the given plane.
+        Return None if there is no intersection or the intersection time is too big.
+        """
+        # plane specified via one point p and a normal vector n
+        # solve the equation (r(t) - p) * n = 0 for t.
+        # (t * v - p) * n = 0
+        # t * v * n - p * n = 0
+        # t = p * n / v * n
+        # TODO: handle case where there is no intersection
+        #       or intersection time is astronomical
+        return planePoint.dot(planeNormal) / self._velocity.dot(planeNormal)
 
 class PathGenerator:
     """
     Randomly generates trajectories for use in event simulation.
     """
     def __init__(self, geometry: DetectorGeometry):
-        pass
+        self._speed_distribution = 1.0
+        self._origin_distribution = numpy.array([0.0, 0.0, 0.0])
+        # hardcoded seed for now TODO: chan0ge
+        self._rng = UniformSolidAngleGenerator(0, geometry.source.direction, geometry.source.opening_angle)
+        self._particle_source = geometry.source.position
 
     def generate_random_path(self) -> ParticlePath:
         """
         Return a randomly generated trajectory through the detector.
         """
+        origin = self._generate_random_origin()
+        velocity = self._generate_random_speed() * self._rng.generate_random_point()
+        return ParticlePath(origin, velocity)
+
+    # random generator for the sake of completeness (?)
+    def _generate_random_speed(self) -> float:
+        if isinstance(self._speed_distribution, float):
+            return self._speed_distribution
+        # error
+        raise NotImplementedError("Only delta distributions are supported for velocities currently.")
+
+    # random generator for the sake of completeness (?)
+    def _generate_random_origin(self) -> float:
+        if isinstance(self._origin_distribution, numpy.ndarray) and self._origin_distribution.shape == (3,):
+            return self._origin_distribution
+        # error
+        raise NotImplementedError("Only delta distributions are supported for origins currently.")
+
+
+##
+## RANDOM GENERATORS
+##
+## using lots of monte carlo because it's fun and simple
+##
+
+
 class UniformSphereGenerator:
     """
-    Allows to randomly sample from a uniform distribution on the unit
-    sphere.
+    Allows to randomly sample from a uniform distribution on the surface of the unit sphere.
     """
     def __init__(self, seed):
         # store for debug purposes
@@ -40,10 +93,50 @@ class UniformSphereGenerator:
         Return a random point on the surface of the unit sphere.
         """
         while True:
-            # generate random point in the unit cube
-            point_in_cube = self._rng.uniform(low=0.0, high=1.0, size=3)
+            # generate random point in the bounding box of the unit sphere
+            point_in_cube = self._rng.uniform(low=-1.0, high=1.0, size=3)
             # check if point lies in the unit sphere
             if numpy.linalg.norm(point_in_cube) < 1:
                 # project onto sphere surface i.e. normalize
                 point_on_sphere = point_in_cube / numpy.linalg.norm(point_in_cube)
                 return point_on_sphere
+            # try again
+
+class UniformSolidAngleGenerator:
+    """
+    Allows to randomly sample from a uniform distribution on a solid
+    angle cutout of the surphace of the unit sphere.  Solid angle cutout
+    means the region bounded by the spheres surfaces' intersection with a
+    cone, whose tip is centered at the spheres center.
+
+    Paramters:
+    ----------
+    seed: Seed for the random generator
+    direction: direction of the solid angles center axis (vector is normalized internally)
+    opening_angle: opening angle from center axis to rim of solid angle cutout (0 < opening_angle < 2 PI)
+    """
+    def __init__(self, seed, direction, opening_angle):
+        # check if opening_angle values are ok
+        if opening_angle < 0 or opening_angle > 2 * numpy.pi:
+            raise ValueError("opening_angle has to be between 0 and 2 PI but is {}.".format(opening_angle))
+        if opening_angle > numpy.pi:
+            raise NotImplementedError("Given opening_angle {} is greater than PI (180°). Only solid angles smaller than half the sphere work right now.".format(opening_angle))
+        self._opening_angle = opening_angle
+        # normalize direction
+        self._direction = direction / numpy.linalg.norm(direction)
+        # initialize inner rng
+        self._seed = seed
+        self._sphere_rng = UniformSphereGenerator(seed)
+
+    def generate_random_point(self):
+        """
+        Return a random point in the on the unit sphere surface and inside the solid angle
+        """
+        while True:
+            # random point on the sphere surface
+            point_on_sphere = self._sphere_rng.generate_random_point()
+            # check if point lies in opening angle, note both point_on_sphere and direction are normalized
+            if point_on_sphere.dot(self._direction) < numpy.cos(self._opening_angle):
+                # point_on_sphere is good
+                return point_on_sphere
+            # try again
--- python/test/test_generation.py
+++ python/test/test_generation.py
@@ -1,5 +1,9 @@
-import generation
+"""
+Tests for the "generation" module
+"""
+
 import numpy
+import generation
 
 def test_unifrom_sphere_generator():
     """
@@ -14,3 +18,67 @@ def test_unifrom_sphere_generator():
         assert point.shape == (3,)
         # verify it's on the surface of the unit sphere
         assert numpy.isclose(numpy.linalg.norm(point), 1)
+
+def test_path_plane_intersection_good():
+    """
+    Test the algorithm which computes the intersectoin between a plane and a straight line.
+    """
+    ##
+    ## TEST CASES with an intersection
+    ##
+
+    # xy-plane with z offset of 10
+    plane_normal = numpy.array([0, 0, 1])
+    plane_point = numpy.array([0, 0, 10])
+    # path from in z direction with unit velocity
+    path_origin = numpy.array([0, 0, 0])
+    path_velocity = numpy.array([0, 0, 1])
+    path = generation.ParticlePath(path_origin, path_velocity)
+    intersection_time = path.intersection_time_with_plane(plane_normal, plane_point)
+    assert intersection_time is not None
+    intersection_point = path.get_position_at_time(intersection_time)
+    assert intersection_point is not None
+    assert numpy.all(numpy.isclose(intersection_point, numpy.array([0, 0, 10])))
+
+    ##
+    ## TEST CASES with no intersection
+    ##
+
+    # xy-plane with z offset of 10
+    plane_normal = numpy.array([0, 0, 1])
+    plane_point = numpy.array([0, 0, 10])
+    # path from in z direction with unit velocity
+    path_origin = numpy.array([0, 0, 0])
+    # negative velocity -> no intersection
+    path_velocity = numpy.array([0, 0, -1])
+    path = generation.ParticlePath(path_origin, path_velocity)
+    intersection_time = path.intersection_time_with_plane(plane_normal, plane_point)
+    assert intersection_time is None
+
+def test_path_generator():
+    """
+    Test the PathGenerator class, which has a public interface.
+    """
+    # pylint: disable=too-few-public-methods,missing-class-docstring
+    class MockGeometry:
+        """
+        small part of DetectorGeometry, for testing purposes
+        """
+        def __init__(self, position, direction, opening_angle):
+            class Source:
+                def __init__(self):
+                    pass
+            self.source = Source()
+            self.source.position = position
+            self.source.direction = direction
+            self.source.opening_angle = opening_angle
+
+    geom = MockGeometry(numpy.array([0, 0, 0]), numpy.array([0, 0, 1]), 5 / 360 * 2 * numpy.pi)
+    gen = generation.PathGenerator(geom)
+    path = gen.generate_random_path()
+
+    # xy-plane with z offset of 10
+    plane_normal = numpy.array([0, 0, 1])
+    plane_point = numpy.array([0, 0, 10])
+    intersection_time = path.intersection_time_with_plane(plane_normal, plane_point)
+    print(intersection_time)
