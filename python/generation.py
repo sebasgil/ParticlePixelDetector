f7fb9afa7f8d812142b6e52f95f424db6fded121
Sebastián Gil <>
2021-01-29 16:19:08 +0100

    Refactor OrientationGenerator to get source opening angle and direction from DetectorGeometry

--- python/generation.py
+++ python/generation.py
@@ -1,5 +1,6 @@
 """Event generation."""
 import numpy  as np
+from common import DetectorGeometry
 
 class OrientationGenerator:
 	"""Generates a random orientation to initialize a particle path"""
@@ -14,26 +15,25 @@ class OrientationGenerator:
             Seed for the internal random generator.
         opening angle: float
             The fixed value for the source specified for the project
-        direction:  3-tuple
+        direction:  array
             The direction along which the source points.
             Held constant at the positive z-axis line of sight.
-            TO-DO: Should this be subsumed under Geometry?
         """
         # store for debug purposes
         self._seed = seed
         # initialize random generator
         self._rng = numpy.random.default_rng(self._seed)
-        self._opening_angle = 10.0
-        self._direction = (0.,0.,1.0)
+        self._opening_angle = DetectorGeometry.source_opening_angle
+        self._direction = DetectorGeometry.source_direction
         
-    def get_random_sphere_vector():
+    def generate_random_sphere_vector():
         '''Draw a random vector form the spherically symmetric 3D
 	normal distribution and normalize to unit length'''
 	vector = np.random.normal(size=3)
 	normalized_vector = vector / np.linalg.norm(vector)
 	return normalized_vector
 
-    def get_solid_angle(direction, opening_angle):
+    def generate_solid_angle():
 	 '''Generate a random sphere vector and check whether its orientation
 	 aligns with that of the opening angle.'''
 	     while True:
@@ -51,21 +51,20 @@ class PathGenerator:
 
         Parameters
         ----------
-	orientation: 3-tuple
+	orientation: array
 		The initial orientation of the particle drawn
-        velocity: 3-tuple
+        velocity: array
         	The initial velocity of the particle stored as a vector
         	Because the detector is massless, velocity remains constant
-        	So the data type is a tuple
         samples: integer
         	the number of time-steps in a particle trajectory
         """
         
         self._orientation = OrientationGenerator.get_solid_angle()
-        self._velocity = velocity
-	self._samples = samples
+        self._velocity = velocity # how is this generated? from where?
+	self._samples = samples # how is this generated? from where?
 
-    def get_coordinates(self, seed):
+    def generate_coordinates(self, seed):
  	"""
 	Fill in an array of the particle's coordinates for the number
 	of samples given
@@ -78,9 +77,8 @@ class PathGenerator:
         	for the number of samples given
         """
         # initialize array of the correct shape and number of time steps
-        self._coordinates = np.zeroes([samples,3])
+        self.coordinates = np.zeroes([samples,3])
         for time_step in np.arange(samples):
-            self._coordinates[time_step + 1] = self._coordinates[time_step] + np.array(self._velocity)
+            self.coordinates[time_step + 1] = self.coordinates[time_step] + np.array(self._velocity)
             # TO-DO: test that this works, especially for final time-steps
-        
-        return self._coordinates
+        return self.coordinates
