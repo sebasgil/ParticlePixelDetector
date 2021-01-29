77764ad875a7d1659d4c417518b8ed50cdc352a7
Sebastián Gil <>
2021-01-29 16:00:26 +0100

    Refactor code by renaming classes and removing redundancies. Ready to test

--- python/generation.py
+++ python/generation.py
@@ -1,9 +1,48 @@
 """Event generation."""
 import numpy  as np
 
+class OrientationGenerator:
+	"""Generates a random orientation to initialize a particle path"""
 
-class ParticlePath:
-    """The path that a single particle takes through the detector."""
+    def __init__(self, seed: int):
+        """
+	Initialize the random number generator      
+
+        Parameters
+        ---------
+        seed: int
+            Seed for the internal random generator.
+        opening angle: float
+            The fixed value for the source specified for the project
+        direction:  3-tuple
+            The direction along which the source points.
+            Held constant at the positive z-axis line of sight.
+            TO-DO: Should this be subsumed under Geometry?
+        """
+        # store for debug purposes
+        self._seed = seed
+        # initialize random generator
+        self._rng = numpy.random.default_rng(self._seed)
+        self._opening_angle = 10.0
+        self._direction = (0.,0.,1.0)
+        
+    def get_random_sphere_vector():
+        '''Draw a random vector form the spherically symmetric 3D
+	normal distribution and normalize to unit length'''
+	vector = np.random.normal(size=3)
+	normalized_vector = vector / np.linalg.norm(vector)
+	return normalized_vector
+
+    def get_solid_angle(direction, opening_angle):
+	 '''Generate a random sphere vector and check whether its orientation
+	 aligns with that of the opening angle.'''
+	     while True:
+		solid_angle = get_random_sphere_vector()
+		if solid_angle.dot(self._direction) < np.cos(self._opening_angle): # note that the second is -0.83; is this a problem?
+		    return random_unit_vector
+
+class PathGenerator:
+    """Generates the path that a single particle takes through the detector."""
 
     def __init__(self, orientation, velocity, samples):
         """
@@ -22,10 +61,9 @@ class ParticlePath:
         	the number of time-steps in a particle trajectory
         """
         
-        self._orientation = GetOrientation.get_solid_angle()
+        self._orientation = OrientationGenerator.get_solid_angle()
         self._velocity = velocity
 	self._samples = samples
-
 
     def get_coordinates(self, seed):
  	"""
@@ -46,50 +84,3 @@ class ParticlePath:
             # TO-DO: test that this works, especially for final time-steps
         
         return self._coordinates
-        
-class GetOrientation:
-	"""Create a random orientation to initialize a particle path"""
-
-
-    def __init__(self, seed: int):
-        """
-	Initialize the random number generator      
-
-        Parameters
-        ---------
-        seed: int
-            Seed for the internal random generator.
-        opening angle: float
-            The fixed value for the source specified for the project
-        direction:  3-tuple
-            The direction along which the source points.
-            Held constant at the positive z-axis line of sight.
-            TO-DO: Should this be subsumed under Geometry?
-        """s
-        # store for debug purposes
-        self._seed = seed
-        # initialize random generator
-        self._rng = numpy.random.default_rng(self._seed)
-        self._opening_angle = 10.0
-        self._direction = (0.,0.,1.0)
-        
-        
-    def get_random_sphere_vector():
-        '''Draw a random vector form the spherically symmetric 3D
-	normal distribution and normalize to unit length'''
-	vector = np.random.normal(size=3)
-	normalized_vector = vector / np.linalg.norm(vector)
-	return normalized_vector
-
-    def get_solid_angle(direction, opening_angle):
-	 '''Generate a random sphere vector and check whether its orientation
-	 aligns with that of the opening angle.'''
-	 opening_angle  = 10.0
-	 direction = np.array([0.,0.,1.0])
-	     while True:
-		solid_angle = get_random_sphere_vector()
-		if solid_angle.dot(self._direction) < np.cos(self._opening_angle): # note that the second is -0.83; is this a problem?
-		    return random_unit_vector
-
-    
-
