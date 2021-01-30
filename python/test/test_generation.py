662547130dc2aa58b04b15fdc8cbd83a953c4115
Sebastián Gil <>
2021-01-30 18:30:41 +0100

    Brainstorm tests for generation module.

--- python/test/test_generation.py
+++ python/test/test_generation.py
@@ -1,8 +1,23 @@
 """Tests for the "generation" module."""
 
-import numpy  # type: ignore
+import numpy  as np # type: ignore
 import pytest
 import generation
 
+def test_random_sphere_vector():
+	"""Docstring"""
+	# check it has non-zero components
+	# check it's normalized
+	# check it's on sphere surface
+	
+def test_solid_angle():
+	"""Docstring"""
+	# check while loop terminates
+	# check that vector is inside desired interval
+	# check that an Exception is raised for wrong vectors
 
-
+def test_particle_path():
+	"""Docstring"""
+	# check that orientation found is inside desired interval
+	# check that coordinates array is not empty
+	# check that generated particle goes through all panes
