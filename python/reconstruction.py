8c9b7ce921fc44b3cd3b310d22f178a92767250b
Massin Guerdi <massinguerdi@web.de>
2021-02-02 13:37:03 +0100

    cleanup / reformat reconstruction, fix lints

--- python/reconstruction.py
+++ python/reconstruction.py
@@ -2,7 +2,9 @@
 Reconstruction of paths from events.
 """
 
+import numpy as np
 from common import DetectorGeometry, Event
+
 
 class ReconstructedPath:
     """
@@ -12,7 +14,7 @@ class ReconstructedPath:
         self.event_id = event_id
         self.direction = direction
         self.centeroid = centeroid
-        
+
 
 class Reconstructor:
     """
@@ -30,41 +32,27 @@ class Reconstructor:
         # TODO: wording
         event: The event whose original path is to be reconstructed.
         """
-        ReconstructedPath = ReconstructedPath(event.get_id(), find_direction(event.activated_pixel_positions(), self.number_of_iterations), find_centroid(event.activated_pixel_positions()))
+        reconstructed_path = ReconstructedPath(
+            event.get_id(),
+            find_direction(
+                event.activated_pixel_positions(),
+                self.number_of_iterations
+            ),
+            find_centroid(event.activated_pixel_positions())
+        )
 
-        return ReconstructedPath
-
-
-
-
-
-
-
-
-
+        return reconstructed_path
 
 
-################################################################################################
-
-import numpy as np
-
-
-#here we have to import some functions from common which can read the activated pixels
-# and convert them into coordinates. This would be the coordinates of the point at 
-# which each pixel would be centered.
-
-pixel_info = np.zeros((5,3))
-
-def find_centroid(pixel_info): #returns centroid
+def find_centroid(pixel_info):  # returns centroid
     centroid = np.mean(pixel_info, axis=0)
     return centroid
 
 
-
-def find_direction(pixel_info, number_of_iterations): #returns direction
+def find_direction(pixel_info, number_of_iterations):  # returns direction
     centroid = find_centroid(pixel_info)
     direction = np.array([1.0, 0.0, 0.0])
-    for j in range(number_of_iterations):
+    for _ in range(number_of_iterations):
         next_direction = np.array([0.0, 0.0, 0.0])
 
         for point in pixel_info:
@@ -75,26 +63,35 @@ def find_direction(pixel_info, number_of_iterations): #returns direction
             next_direction += dot*point_vec
 
         norm = np.dot(next_direction, next_direction)
-    
-        direction = (1/(norm**(0.5)))*next_direction 
+
+        direction = (1/(norm**(0.5)))*next_direction
+
     return direction
 
+
 def distance_func(point, point_on_line, dcs_of_line):
-    centroid = point_on_line #any point on the line
-    direction = dcs_of_line #direction cosines of the line
+    centroid = point_on_line  # any point on the line
+    direction = dcs_of_line  # direction cosines of the line
     vector_to_centroid = centroid - point
     area_of_parallelogram = np.cross(vector_to_centroid, direction)
 
-    distance = (np.dot(area_of_parallelogram,area_of_parallelogram))/(np.dot(direction,direction))
+    distance = (
+        np.dot(area_of_parallelogram, area_of_parallelogram)
+        / np.dot(direction, direction)
+    )
+
     distance = (abs(distance))**(0.5)
+
     return distance
 
 
 def measure(pixel_info):
     sum_distances = 0.0
     for point in pixel_info:
-        sum_distances += distance_func(point, find_centroid(pixel_info), find_direction(pixel_info, 10))
+        sum_distances += distance_func(
+            point,
+            find_centroid(pixel_info),
+            find_direction(pixel_info, 10)
+        )
 
     return sum_distances
-
- 
