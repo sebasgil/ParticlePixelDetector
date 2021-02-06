Fix orientation vector to evaluate angles in radians!

python/generation.py
@@ -35,8 +35,8 @@ class OrientationGenerator:
 	def generate_orientation_vector(self):
 		'''Generate a random sphere vector and check whether its orientation aligns with that of the opening angle.'''
 		while True:
-			orientation_vector = self.get_random_sphere_vector()
-			if orientation_vector.dot(self._direction) < np.cos(self._opening_angle  * np.pi/ 180):
+			orientation_vector = self.generategener_random_sphere_vector()
+			if orientation_vector.dot(self._direction) < np.cos(self._opening_angle):
 				return orientation_vector
 
 class PathGenerator: